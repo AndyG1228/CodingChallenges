189da8b2f072f1fc0538b66854497b06a2a7ac7a
AndyG1228 <606925@student.dusd.net>
2023-12-15 13:54:57 -0800

    Please
    yes

---
+++ CodingChallengesGallardo/CodingChallengesGallardo/Program.cs
@@ -0,0 +1,465 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata.Ecma335;
+using System.Security;
+
+class CodingChallenges
+{
+
+    static void Main(string[] args)
+    {
+        Project();
+    }
+
+    static void Project()
+    {
+        while (true)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Please insert an integer between 1 and 26 to begin the program, or press 0 to exit.");
+            Console.WriteLine("[1] - Sum (Add two numbers)");
+            Console.WriteLine("[2] - Convert (minutes into seconds)");
+            Console.WriteLine("[3] - PlusOne (Takes a number and adds one)");
+            Console.WriteLine("[4] - CircuitPower (Returns power with voltage and current)");
+            Console.WriteLine("[5] - CalcAge (Converts years to days)");
+            Console.WriteLine("[6] - triArea (Provides area through base and height)");
+            Console.WriteLine("[7] - lessThanOrEqualToZero ('Is a number...?')");
+            Console.WriteLine("[8] - lessThan100 ('Is the sum of two numbers...?)");
+            Console.WriteLine("[9] - IsEqual (Determines if two integers are equal)");
+            Console.WriteLine("[10] - GiveMeSomething (Returns 'something' and the argument)");
+            Console.WriteLine("[11] - Reverse (Reverses a boolean value)");
+            Console.WriteLine("[12] - howManySeconds (Converts hours to seconds)");
+            Console.WriteLine("[13] - SumPolygon (The sum of internal angles of a _ sided polygon)");
+            Console.WriteLine("[14] - nameString (_Edabbit)");
+            Console.WriteLine("[15] - And (Returns true if both values are true)");
+            Console.WriteLine("[16] - Points (Total worth of given 3 pointers and 2 pointers)");
+            Console.WriteLine("[17] - FindPerimeter (Adds lengths and widths of a rectangle)");
+            Console.WriteLine("[18] - HelloName (Hello _)");
+            Console.WriteLine("[19] - animals (Sum of legs of given chickens, cows, and pigs)");
+            Console.WriteLine("[20] - FootballPoints(Points from winds, draws, and losses)");
+            Console.WriteLine("[21] - MonthName (Select a month with an integer)");
+            Console.WriteLine("[22] - FindMinMax (Find minimum and xaximum values of an array)");
+            Console.WriteLine("[23] - getAbsSum (Sum of the absolute value of each integer)");
+            Console.WriteLine("[24] - CaluculateExponent (Calculates a base with an exponent)");
+            Console.WriteLine("[25] - MultiplyByLength (Multiplies all values in the array by length)");
+            Console.WriteLine("[26] - HammingDistance (Computes the number of differing characters between two strings)");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Challenge1();
+                    break;
+                default:
+                    Console.WriteLine("Please insert an acceptable choice");
+                    break;
+            }
+
+        }
+    }    
+        
+        
+        static void Challenge1()
+        {
+            //Challenge 1
+            Console.WriteLine("The sum of 3 and 9 is " + Sum(3, 9));
+        }
+        
+
+        //Challenge 2
+        Console.WriteLine("In 6 minutes there are " + ToSec(6) + " seconds");
+        
+        //Challenge 3
+        Console.WriteLine("What is your original number?");
+        int oops = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("When that number increases by 1, we get " + PlusOne(oops));
+        
+        //Challenge 4
+        Console.WriteLine("The current Circuit Power is at " + Breaker(230,10) + " Watts");
+
+        //Challenge 5
+        Console.WriteLine("How old am I, creator?");
+        int five = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("I'm " + five + " years old, meaning that I am also " + Age(five) + " days old");
+
+        //Challenge 6
+        Console.WriteLine("How long is the bottom of your triange?");
+        int six = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("How tall is your triange?");
+        int seven = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("The area of your triangle is " + triArea(six, seven) + " inches squared");
+
+        //Challenge 7
+        Console.WriteLine("Insert a value");
+        int eight = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("This is " + lessThanOrEqualToZero(eight));
+
+        //Challenge 8
+        Console.WriteLine("Insert the first value");
+        int nine = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Insert the second value");
+        int ten = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("This is " + lessThan100(nine,ten));
+        
+        //Challenge 9
+        Console.WriteLine("Insert a number");
+        int eleven = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Insert the same number");
+        int twelve = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("This is " + IsEqual(eleven,twelve));
+        
+        //Challenge 10
+        Console.WriteLine("Give me something");
+        string a = Console.ReadLine();
+        Console.WriteLine(GiveMeSomething(a));
+        
+        //Challenge 11
+        Console.WriteLine("Is this true or false?");
+        bool a = Convert.ToBoolean(Console.ReadLine());
+        Console.WriteLine("It's actually " + Reverse(a));
+        Console.WriteLine("What is it now?");
+        bool b = Convert.ToBoolean(Console.ReadLine());
+        Console.WriteLine("It's " + Reverse(b) + "....");
+        
+        //Challenge 12
+        Console.WriteLine("If you tell me a number of hours, I'll tell you how long that is in seconds!");
+        int thirteen = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("There are " + howManySeconds(thirteen) + " seconds in that many hours.");
+        
+        //Challenge 13
+        Console.WriteLine("How many sides does your polygon have?");
+        int fourteen = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("The sum of the angles of your polygon is " + SumPolygon(fourteen) + " degrees.");
+        
+        //Challenge 14
+        Console.WriteLine("Give me your name");
+        string b = Console.ReadLine();
+        Console.WriteLine(nameString(b));
+
+        //Challenge 15
+        Console.WriteLine("Is this true?");
+        bool c = Convert.ToBoolean(Console.ReadLine());
+        Console.WriteLine("Then this must be true too?");
+        bool d = Convert.ToBoolean(Console.ReadLine());
+        Console.WriteLine("It's " + And(c, d));
+
+        //Challenge 16
+        Console.WriteLine("How many three pointers were scored?");
+        int fifteen = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("And how many two pointerss were scored?");
+        int sixteen = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Then they scored a total of " + Points(fifteen, sixteen) + " points");
+        
+        //Challenge 17
+        Console.WriteLine("What is the length of your rectangle?");
+        int seventeen = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("And what is its width?");
+        int eighteen = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Then the perimeter is " + FindPerimeter(seventeen, eighteen) + " units");
+
+        //Challenge 18
+        Console.WriteLine("Could I get your name again?");
+        string c = Console.ReadLine();
+        Console.WriteLine(HelloName(c));
+        
+        //Challenge 19
+        Console.WriteLine("How many chickens do you have?");
+        int nineteen = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("And how many cows do you have?");
+        int twenty = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("And your pigs?");
+        int twentyone = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Then all your animals together have " + animals(nineteen, twenty, twentyone) + " legs!");
+        Console.WriteLine("Why're you looking at me like that?");
+        
+        //Challenge 20
+        Console.WriteLine("How many wins does your team have?");
+        int twentytwo = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("And how many draws does your team have?");
+        int twentythree = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("And your team's losses?");
+        int twentyfour = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Then your team has obtained " + FootballPoints(twentytwo, twentythree, twentyfour) + " points!");
+        
+        //Challenge 21
+        Console.WriteLine("In digit form, what month were you born in?");
+        int twentyfive = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(MonthName(twentyfive) + ", then?");
+        
+        //Challenge 22
+        int[] a = { 0, 0, 0, 0, 0, };
+        Console.WriteLine("Enter the first value");
+        a[0] = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the second value");
+        a[1] = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the third value");
+        a[2] = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the fourth value");
+        a[3] = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the fifth value");
+        a[4] = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(FindMinMax(a));
+        
+        //Challenge 23
+        int[] b = { 0, 0, 0, 0, 0, };
+        Console.WriteLine("Enter the first value");
+        b[0] = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the second value");
+        b[1] = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the third value");
+        b[2] = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the fourth value");
+        b[3] = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the fifth value");
+        b[4] = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(getAbsSum(b));
+        
+        //Challenge 24
+        Console.WriteLine("What is the value of your base value?");
+        int twentysix = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("What is the value of your exponent?");
+        int twentyseven = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(CalculateExponent(twentysix,twentyseven) + " should be the value of your number");
+        
+        //Challenge 25
+        Console.WriteLine("Give me the amount of numbers you'd like in your array.");
+        int twentyeight = Convert.ToInt32(Console.ReadLine());
+        int[] c = new int[twentyeight];
+        for (int i = 0; i < c.Length; i++)
+        {
+            Console.WriteLine("Enter an integer");
+            int a = Convert.ToInt32(Console.ReadLine());
+            c[i] = a;
+        }
+        Console.WriteLine("Your new array is " + MultiplyByLength(c));
+        */
+        //Challenge  26
+        Console.WriteLine("What is the first string?");
+        string d = Console.ReadLine();
+        Console.WriteLine("What is the second string?");
+        string e = Console.ReadLine();
+
+        if (d.Length != e.Length)
+        {
+            Console.WriteLine("Please insert strings with the same length");
+        }
+        else
+        {
+            Console.WriteLine(HammingDistance(e, d));
+            
+        }
+    }
+    static int Sum(int a, int b)
+    {
+        int sum = a + b;
+        return sum;
+    }
+    public static int ToSec(int minutes)
+    {
+        int seconds = minutes * 60;
+        return seconds;
+    }
+
+    public static int PlusOne(int original)
+    {
+        int total = original + 1;
+        return total;
+    }
+
+    public static int Breaker(int voltage, int current)
+    {
+        int power = voltage * current;
+        return power;
+    }
+
+    public static int Age(int years)
+    {
+        int days = years * 365;
+        return days;
+    }
+
+    public static int triArea(int bottom, int height)
+    {
+        int area = (bottom * height) / 2;
+        return area;
+    }
+
+    public static bool lessThanOrEqualToZero(int wow)
+    {
+        if (wow <= 0)
+        {
+            return true;
+        }
+
+        else
+        {
+            return false;
+        }
+
+    }
+
+    public static bool lessThan100(int woah, int what)
+    {
+        if ((woah + what) < 100)
+        {
+            return true;
+        }
+
+        else
+        {
+            return false;
+        }
+
+    }
+
+    public static bool IsEqual(int huh, int how)
+    {
+        if (huh == how)
+        {
+            return true;
+        }
+
+        else
+        {
+            return false;
+        }
+
+    }
+
+    public static string GiveMeSomething(string words)
+    {
+        string sentence = "something " + words;
+        return sentence;
+    }
+
+    public static bool Reverse(bool oppositeday)
+    {
+        if (oppositeday == true)
+        {
+            return false;
+        }
+
+        else
+        {
+            return true;
+        }
+    }
+
+    public static int howManySeconds(int hours)
+    {
+        int seconds = hours * 3600;
+        return seconds;
+    }
+
+    public static int SumPolygon(int sides)
+    {
+        int angle = (sides - 2) * 180;
+        return angle;
+    }
+
+    public static string nameString(string name)
+    {
+        string nameplus = name + "Edabbit";
+        return nameplus;
+    }
+
+    public static bool And(bool a, bool b)
+    {
+        if (a && b)
+            return true;
+        else return false;
+
+    }
+    public static int Points(int threepointers, int twopointers)
+    {
+        int total = (threepointers * 3) + (twopointers * 2);
+        return total;
+    }
+    public static int FindPerimeter(int length, int  width)
+    {
+        int perimeter = (length * 2) + (width * 2);
+        return perimeter;
+    }
+
+    public static string HelloName(string name)
+    {
+        string phrase = "Hello " + name + "!";
+        return phrase;
+    }
+
+    public static int animals(int chickens, int cows, int pigs)
+    {
+        int legs = (chickens * 2) + (cows * 4) + (pigs * 4);
+        return legs;
+    }
+
+    public static int FootballPoints(int wins, int draws, int losses)
+    {
+        int totalpoints = (3 * wins) + (1 * draws) + (0 * losses);
+        return totalpoints;
+    }
+
+    public static string MonthName(int digit)
+    {
+        string[] month = {"January", "February", "March", "April", "May", "June", "August",
+            "September", "October", "Novermber", "December",};
+
+        if(digit > 0 && digit <= 12)
+        {
+            digit--;
+            return month[digit];
+        }
+
+        else
+        {
+            return "I don't believe you understood me...";
+        }
+    }
+
+    public static string FindMinMax(int [] numbers)
+    {
+        int min = numbers.Min();
+        int max = numbers.Max();
+        Console.WriteLine("The minimum number is " + min + " and the maximum number is " + max);
+        return "["+ min + "," + max +"]";
+    }
+
+    public static int getAbsSum(int[] integers)
+    {
+        int sum = Math.Abs(integers[0]) + Math.Abs(integers[1]) + Math.Abs(integers[2]) + Math.Abs(integers[3]) +
+                  Math.Abs(integers[4]);
+        return sum;
+    }
+
+    public static long CalculateExponent(int foundation, int exponent)
+    {
+        long total = (long)Math.Pow(foundation, exponent);
+        return total;
+    }
+
+    public static string MultiplyByLength(int[] multiplicand)
+    {
+        string a = "";
+        for (int i = 0;  i < multiplicand.Length; i++)
+        {
+            a += "[" + (multiplicand[i] * multiplicand.Length) + "]";
+        }
+        return a;
+    }
+
+    public static int HammingDistance(string first, string second)
+    {
+        int distance = 0;
+
+        for (int i = 0; i <first.Length; i++)
+            if (first[i] != second[i])
+            {
+            distance++;
+            }
+        return distance;
+    }
+
+   
+}
